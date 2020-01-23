c77d55aaaeeb7c912ad10e599afdd2544ad2d48e
Angela <angyvazquezdel97>
2020-01-23 19:20:04 +0100

    Proyecto en VS

---
+++ ProyectoERP_API/ProyectoERP_API_DAL/Connection/clsMyConnection.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+// Esta clase contiene los métodos necesarios para trabajar con el acceso a una base de datos SQL Server
+//PROPIEDADES
+//   _server: cadena 
+//   _database: cadena, básica. Consultable/modificable.
+//   _user: cadena, básica. Consultable/modificable.
+//   _pass: cadena, básica. Consultable/modificable.
+
+// MÉTODOS
+//   Function getConnection() As SqlConnection
+//       Este método abre una conexión con la base de datos. Lanza excepciones de tipo: SqlExcepion, InvalidOperationException y Exception.
+//   
+//   Sub closeConnection(ByRef connection As SqlConnection)
+//       Este método cierra una conexión con la base de datos. Lanza excepciones de tipo: SqlExcepion, InvalidOperationException y Exception.
+//
+
+
+namespace ProyectoERP_API_DAL.Connection
+{
+    public class clsMyConnection
+    {
+        //Atributos
+        public String server { get; set; }
+        public String dataBase { get; set; }
+        public String user { get; set; }
+        public String pass { get; set; }
+
+        //Constructores
+
+        public clsMyConnection()
+        {
+            // this.server = "(local)";
+            this.server = "personas.database.windows.net";
+            //nombre de la base de datos
+            this.dataBase = "Personas";
+            this.user = "angela";
+            this.pass = "Mitesoro123";
+
+        }
+        //Con parámetros por si quisiera cambiar las conexiones
+        public clsMyConnection(String server, String database, String user, String pass)
+        {
+            this.server = server;
+            this.dataBase = database;
+            this.user = user;
+            this.pass = pass;
+        }
+
+
+        //METODOS
+
+        /// <summary>
+        /// Método que abre una conexión con la base de datos
+        /// </summary>
+        /// <pre>Nada.</pre>
+        /// <returns>Una conexión con la base de datso</returns>
+        public SqlConnection getConnection()
+        {
+            SqlConnection connection = new SqlConnection();
+
+            try
+            {
+               
+                //connection.ConnectionString = string.Format("server={0};database={1};uid={2};pwd={3};", server, dataBase, user, pass);
+                connection.ConnectionString = $"server={server};database={dataBase};uid={user};pwd={pass};";
+                connection.Open();
+            }
+            catch (SqlException)
+            {
+                throw;
+            }
+
+            return connection;
+
+        }
+
+
+
+
+        /// <summary>
+        /// Este metodo cierra una conexión con la Base de datos
+        /// </summary>
+        /// <post>The connection is closed.</post>
+        /// <param name="connection">La entrada es la conexión a cerrar
+        /// </param>
+        public void closeConnection(ref SqlConnection connection)
+        {
+            try
+            {
+                connection.Close();
+            }
+            catch (SqlException)
+            {
+                throw;
+            }
+            catch (InvalidOperationException)
+            {
+                throw;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+    }
+
+}
