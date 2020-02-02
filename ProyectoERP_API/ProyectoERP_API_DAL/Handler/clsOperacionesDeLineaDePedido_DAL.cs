82ed9e168e0f7efa057181fc2266d678d931b1d7
dianbero <dianbero@gmail.com>
2020-02-02 14:55:21 +0100

    Métodos 7-9 DAL y BL
    7-actualizar un pedido por id
    8-actualizar una linea de pedido de un producto de un pedido
    9-insertar un pedido nuevo (insertar líneas pedido según el código de pedido)

---
+++ ProyectoERP_API/ProyectoERP_API_DAL/Handler/clsOperacionesDeLineaDePedido_DAL.cs
@@ -0,0 +1,181 @@
+﻿using ProyectoERP_API_DAL.Connection;
+using ProyectoERP_API_Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoERP_API_DAL.Handler
+{
+    public class clsOperacionesDeLineaDePedido_DAL
+    {
+        /// <summary>
+        /// Método que obtiene una línea de pedido según el código de producto y código de Pedido
+        /// </summary>
+        /// <param name="codigoProducto">Entero con código del producto cuya línea se consulta</param>
+        /// <param name="codigoPedido">Entero con código del pedido al que pertenece la línea a consultar</param>
+        /// <returns>clsLineaPedido lineaPedido</returns>
+        public clsLineaPedido GetLineaPedidoPorIdProductoIdPedido(int codigoProducto, int codigoPedido)
+        {
+            clsMyConnection clsMyConnection = new clsMyConnection();
+            SqlConnection connection = null;
+            SqlCommand command = new SqlCommand();
+            SqlDataReader reader = null;
+            clsLineaPedido lineaPedido = new clsLineaPedido();
+
+            try
+            {
+                connection = clsMyConnection.getConnection();
+                command.Connection = connection;
+
+                command.Parameters.Add("@codigoProducto", System.Data.SqlDbType.Int).Value = codigoProducto;
+                command.Parameters.Add("@codigoPedido", System.Data.SqlDbType.Int).Value = codigoPedido;
+                command.CommandText = "SELECT * FROM ERP_LineaPedidos WHERE CodigoProducto = @codigoProducto AND  CodigoPedido = @codigoPedido";
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        lineaPedido.CodigoProducto = (int)reader["CodigoProducto"];
+                        lineaPedido.CodigoPedido = (int)reader["CodigoPedido"];
+                        lineaPedido.Cantidad = (int)reader["Cantidad"];
+                        lineaPedido.PrecioUnitario = (int)reader["PrecioUnitario"];
+                        lineaPedido.PrecioUnitario = (double)reader["PrecioUnitario"];
+                        lineaPedido.Divisa = (string)reader["Divisa"];
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (reader != null)
+                {
+                    reader.Close();
+                }
+                if (connection != null)
+                {
+                    clsMyConnection.closeConnection(ref connection);
+                }
+            }
+
+            return lineaPedido;
+        }
+
+
+
+        /// <summary>
+        /// Método que actualiza un línea de pedido según el código de producto y el código de pedido
+        /// </summary>
+        /// <param name="lineaPedidoAModificar">Objeto clsLineaPedido que se va a modificar</param>
+        /// <returns>int filasAfectadas</returns>
+        public int ActualizarLineaPedidoPorIdProductoIdPedido(clsLineaPedido lineaPedidoAModificar)
+        {
+            clsMyConnection clsMyConnection = new clsMyConnection();
+            SqlConnection connection = null;
+            SqlCommand command = new SqlCommand();
+            int filasAfectadas = 0;
+
+            try
+            {
+                connection = clsMyConnection.getConnection();
+                command.Connection = connection;
+                command.Parameters.Add("@codigoProducto", System.Data.SqlDbType.Int).Value = lineaPedidoAModificar.CodigoProducto;
+                command.Parameters.Add("@codigoPedido", System.Data.SqlDbType.Int).Value = lineaPedidoAModificar.CodigoPedido;
+                command.Parameters.Add("@cantidad", System.Data.SqlDbType.TinyInt).Value = lineaPedidoAModificar.Cantidad;
+                command.Parameters.Add("@precioUnitario", System.Data.SqlDbType.Money).Value = lineaPedidoAModificar.PrecioUnitario;
+                command.Parameters.Add("@divisa", System.Data.SqlDbType.VarChar).Value = lineaPedidoAModificar.Divisa;
+
+                command.CommandText = "UPDATE ERP_LineaPedidos SET " +
+                                        "CodigoPedido = @codigoPedido, " +
+                                        "Cantidad = @cantidad, " +
+                                        "PrecioUnitario = @precioUnitario, " +
+                                        "Divisa = " +
+                                        "WHERE CodigoProducto = @codigoProducto AND CodigoPedido = @codigoPedido";
+
+                filasAfectadas = command.ExecuteNonQuery();
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (connection != null)
+                {
+                    clsMyConnection.closeConnection(ref connection);
+                }
+            }
+
+            return filasAfectadas;
+
+        }
+
+        /// <summary>
+        /// Método que inserta líneas de pedido según el código de un nuevo pedido
+        /// </summary>
+        /// <param name="codigoNuevoPedido">int que indica el código del nuevo pedido</param>
+        /// <param name="nuevasLineasPedido">Listado List<clsLineaPedido> con las lineas de pedido a insertar</param>
+        /// <returns>int totalFilasAfectadas</returns>
+        public int InsertarLineasPedidoSegunCodigoDePedido(int codigoNuevoPedido, List<clsLineaPedido> nuevasLineasPedido)
+        {
+            clsMyConnection clsMyConnection = new clsMyConnection();
+            SqlConnection connection = null;
+            SqlCommand command = new SqlCommand();
+            int filasAfectadasPorInsercion = 0;
+            int totalFilasAfectadas = 0;
+
+            //Asigno el codigo del pedido a las líneas del pedido
+            foreach (clsLineaPedido linea in nuevasLineasPedido)
+            {
+                linea.CodigoPedido = codigoNuevoPedido;
+            }
+
+            try
+            {
+                connection = clsMyConnection.getConnection();
+                command.Connection = connection;
+
+                foreach (clsLineaPedido linea in nuevasLineasPedido)
+                {
+                    command.Parameters.Add("@codigoProducto", System.Data.SqlDbType.Int).Value = linea.CodigoProducto;
+                    command.Parameters.Add("@codigoProducto", System.Data.SqlDbType.Int).Value = linea.CodigoPedido;
+                    command.Parameters.Add("@cantidad", System.Data.SqlDbType.TinyInt).Value = linea.Cantidad;
+                    command.Parameters.Add("@precioUnitario", System.Data.SqlDbType.Money).Value = linea.PrecioUnitario;
+                    command.Parameters.Add("@divisa", System.Data.SqlDbType.Money).Value = linea.Divisa;
+
+                    command.CommandText = "INSERT INTO ERP_LineaPedidos(CodigoProducto, CodigoPedido, Cantidad, PrecioUnitario, Divisa) " +
+                                            "VALUES(@codigoProducto, @codigoProducto, @cantidad, @precioUnitario, @divisa)";
+
+                    filasAfectadasPorInsercion = command.ExecuteNonQuery();
+
+                    //Si filasAfectadasPorInsercion es 1, la inserción será exitosa
+                    if (filasAfectadasPorInsercion == 1)
+                    {
+                        //Para comprobar más tarde si totalFilasAfectadas es igual a la cantidad de líneas del listado, entonces la inserción del pedido será exitosa
+                        totalFilasAfectadas++;
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if(connection != null)
+                {
+                    clsMyConnection.closeConnection(ref connection);
+                }
+            }
+
+            return totalFilasAfectadas;
+        }
+
+    }
+}
---
+++ ProyectoERP_API/ProyectoERP_API_DAL/Handler/clsOperacionesDePedidos_DAL.cs
@@ -0,0 +1,154 @@
+﻿using ProyectoERP_API_DAL.Connection;
+using ProyectoERP_API_Entities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoERP_API_DAL.Handler
+{
+    public class clsOperacionesDePedidos_DAL
+    {
+        /// <summary>
+        /// Método que obtiene un pedido determinado según su código
+        /// </summary>
+        /// <param name="codigoPedido">entero con código del pedido a obtener</param>
+        /// <returns>clsPedido pedidoNuevo</returns>
+        public clsPedido GetPedidoPorId(int codigoPedido)
+        {
+            clsMyConnection clsMyConnection = new clsMyConnection();
+            SqlConnection connection = null;
+            SqlCommand command = new SqlCommand();
+            SqlDataReader reader = null;
+            clsPedido pedido = new clsPedido();
+            try
+            {
+                connection = clsMyConnection.getConnection();
+                command.Connection = connection;
+
+                command.Parameters.Add("@codigo", System.Data.SqlDbType.Int).Value = codigoPedido;
+
+                command.CommandText = "SELECT* FROM ERP_Pedidos WHERE Codigo = @codigo";
+
+                reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        pedido.Codigo = (int)reader["Codigo"];
+                        pedido.Estado = (string)reader["Estado"];
+                        pedido.FechaPedido = (DateTime)reader["FechaPedido"];
+                        pedido.FechaRecepcion = (DateTime)reader["FechaRecepcion"];
+                    }
+                }
+                
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (reader != null)
+                {
+                    reader.Close();
+                }
+                if (connection != null)
+                {
+                    clsMyConnection.closeConnection(ref connection);
+                }
+            }
+
+            return pedido;
+        }
+
+
+        /// <summary>
+        /// Método que actualiza un pedido determinado según su Id
+        /// </summary>
+        /// <param name="pedidoParaModificar">Objeto clsPedido que se va a modificar</param>
+        /// <returns>int filasAfectadas</returns>
+        public int ActualizarPedidoPorId(clsPedido pedidoParaModificar)
+        {
+            clsMyConnection clsMyConnection = new clsMyConnection();
+            SqlConnection connection = null;
+            SqlCommand command = new SqlCommand();
+            int filasAfectadas = 0; 
+
+            try
+            {
+                connection = clsMyConnection.getConnection();
+                command.Connection = connection;
+
+                command.Parameters.Add("@codigo", System.Data.SqlDbType.Int).Value = pedidoParaModificar.Codigo;
+                command.Parameters.Add("@estado", System.Data.SqlDbType.VarChar).Value = pedidoParaModificar.Estado;
+                command.Parameters.Add("@fechaPedido", System.Data.SqlDbType.DateTime).Value = pedidoParaModificar.FechaPedido;
+                command.Parameters.Add("@fechaRecepcion", System.Data.SqlDbType.DateTime).Value = pedidoParaModificar.FechaRecepcion;
+
+                command.CommandText = "UPDATE ERP_Pedidos SET Estado = @estado, FechaPedido = @fechaPedido, FechaRecepcion= @fechaRecepcion WHERE Codigo = @codigo";
+
+                filasAfectadas = command.ExecuteNonQuery();
+
+
+            }catch(Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (clsMyConnection != null)
+                {
+                    clsMyConnection.closeConnection(ref connection);
+                }
+            }
+
+            return filasAfectadas;
+
+        }
+
+        /// <summary>
+        /// Método que crea un nuevo pedido llamando a procedimiento en BBDD y devuelve el código del pedido insertado
+        /// </summary>
+        /// <returns>int codigoNuevoPedido</returns>
+        public int InsertarNuevoPedidoDevolviendoSuCodigo() //No recibe parámetros, porque en BBDD crea uno con los mismos datos de base 
+        {
+            clsMyConnection clsMyConnection = new clsMyConnection();
+            SqlConnection connection = null;
+            SqlCommand command = new SqlCommand();
+            int codigoNuevoPedido = 0;
+
+            try
+            {
+                connection = clsMyConnection.getConnection();
+                command.Connection = connection;
+                //Se le pasa el nombre del procedimiento a ejecutar
+                command.CommandText = "crearPedido";
+                //Para que llame al procedimiento almacenado
+                command.CommandType = CommandType.StoredProcedure;
+
+                //Devuelve la primera columna de la primera fila insertada, en nuestro caso el código del pedido
+                codigoNuevoPedido = Convert.ToInt32(command.ExecuteScalar());
+
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (connection != null)
+                {
+                    clsMyConnection.closeConnection(ref connection);
+                }
+            }
+
+            return codigoNuevoPedido;
+        }
+
+        
+    }
+}
