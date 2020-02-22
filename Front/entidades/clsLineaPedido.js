fe262fb7858717d5a0647877d99ab17f8875b030
Angela <angyvazquezdel97>
2020-02-22 19:34:40 +0100

    entidades JS

---
+++ Front/entidades/clsLineaPedido.js
@@ -0,0 +1,9 @@
+class clsLineaPedido {
+    constructor(codigoProducto, codigoPedido, cantidad, precioUnitario, divisa) {
+        this.codigoProducto = codigoProducto;
+        this.codigoPedido = codigoPedido;
+        this.cantidad = cantidad;
+        this.precioUnitario = precioUnitario;
+        this.divisa = divisa;
+    }
+}
